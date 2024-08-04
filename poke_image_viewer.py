08efd2c4983c107c01e7d6fda338f85110aa05b2
DennisDam439 <dend@flemingcollege.ca>
2024-08-04 19:28:53 -0400

    ##image dir & icon path ##### to populate widget
    ASFD

--- poke_image_viewer.py
+++ poke_image_viewer.py
@@ -9,20 +9,26 @@ Usage:
 from tkinter import *
 from tkinter import ttk
 import os
+import poke_api
 
 # Get the script and images directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
 
 # TODO: Create the images directory if it does not exist
+if not os.path.exists(images_dir):
+    os.makedir(images_dir)
 
 # Create the main window
 root = Tk()
 root.title("Pokemon Viewer")
 
 # TODO: Set the icon
+icon_path = os.path.join(script_dir, 'icon.ico')
+root.iconbitmap(icon_path)
 
 # TODO: Create frames
+
 
 # TODO: Populate frames with widgets and define event handler functions
 
