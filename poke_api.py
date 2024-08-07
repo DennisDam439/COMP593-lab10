af9ec08d820f5483ab1e1213ae4081ef0f1278fe
DennisDam439 <dend@flemingcollege.ca>
2024-08-07 11:35:14 -0400

    Update poke_api.py

--- poke_api.py
+++ poke_api.py
@@ -3,6 +3,8 @@ Library for interacting with the PokeAPI.
 https://pokeapi.co/
 '''
 import requests
+import os 
+import image_lib
 
 POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
@@ -47,7 +49,21 @@ def get_pokemon_info(pokemon):
         print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
 
     # TODO: Define function that gets a list of all Pokemon names from the PokeAPI
-
+def get_pokemon_names (limit=100000, offset= 0):
+    print (f'Getting list of Pokemon names....', end='')
+    params = {
+        'limit ' : limit,
+        'offset' : offset 
+    }
+    resp_msg = requests.get (POKE_API_URL, params=params)
+    if resp_msg.status_code== requests.codes.ok:
+        print('Success')
+        resp_dict = resp_msg.jsob()
+        return [p['name'] for p in resp_dict['results']]
+    else:
+        print('Failure')
+        print (f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
+   
     # TODO: Define function that downloads and saves Pokemon artwork
 
 if __name__ == '__main__':
